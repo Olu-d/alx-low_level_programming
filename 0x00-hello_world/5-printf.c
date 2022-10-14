6966edf36b348f4341ea6fe8d08e724df84ffd2a
Olu-D <tunsmen13@icloud.com>
2022-10-14 17:31:10 +0000

    Prints text with printf function in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-This function prints with printf
+* main - This function prints with printf
+*
+* Return: 0 (success)
 */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
