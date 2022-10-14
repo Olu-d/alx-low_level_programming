8ebee1085f66175990c5e2e120c79268a1f19b97
Olu-D <tunsmen13@icloud.com>
2022-10-14 16:53:06 +0000

    Prints text with put function in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 /* C program to show the use of puts */
 #include <stdio.h>
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Description - 'This is just a comment to keep Betty happy'
+ *
+ *Return: Always return 0 (success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
