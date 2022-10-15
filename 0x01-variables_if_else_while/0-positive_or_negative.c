0f4afd950626935af78a99ccd9821e9d5ee829c4
Olu-D <tunsmen13@icloud.com>
2022-10-15 21:41:08 +0000

    positive, negative or zero from random input

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,26 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+  * main - Returns status of random n (positive, negative, zero)
+  * Return: 0 (success)
+*/
 int main(void)
 {
 int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n<0)
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
 	{
 		printf("%d is negative\n", n);
-	}
-	else 
-	{
-		printf("%d is equal to zero\n", n);
 	}
 	return (0);
 }
