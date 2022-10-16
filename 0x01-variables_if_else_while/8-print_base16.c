ed39029550907f3daa2f975f68f8e8acc06de1cc
Olu-D <tunsmen13@icloud.com>
2022-10-16 15:51:28 +0000

    print all hexadecimal numbers (base16) using putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,12 @@ int main(void)
 	int b10 = 0;
 
 	for (b16 = 0; b16 <= 15; b16++)
+	{
 	/* printf("%x", b16); */
 		b10 = printf("%x", b16); /*assign int value to b10*/
 		int putchar(int b10);
-
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 
 
