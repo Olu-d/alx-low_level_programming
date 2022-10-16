b3202dbae8c264601219a086737b5810aa7c584e
Olu-D <tunsmen13@icloud.com>
2022-10-16 21:51:43 +0000

    Print 1-99, skip repeat combinations with nested for loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 	int first_d;
 	int sec_d;
 
-	for (first_d ='0'; first_d <= '9'; first_d++)
+	for (first_d = '0'; first_d <= '9'; first_d++)
 	{
 		for (sec_d = (first_d + 1); sec_d <= '9'; sec_d++)
 		{
 			putchar(first_d);
 			putchar(sec_d);
-			
-			if(!(first_d == '8' && sec_d == '9'))
+
+			if (!(first_d == '8' && sec_d == '9'))
 			{
 				putchar(',');
 				putchar(' ');
