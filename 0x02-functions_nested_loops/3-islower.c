826b06b78b97ae863d177d93fb2f675bc13661d8
Olu-D <tunsmen13@icloud.com>
2022-10-18 21:37:54 +0000

    Check for lowercase xter. Return 1 else 0

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
 #include"main.h"
+
+/**
+  * _islower - This function returns 1 if input alphabet is lowercase
+  * Otherwise, it returns 0.
+  * @c: Input alphabet between a and z
+  * Return: No main, so no return statement.
+  *
+ */
 
 int _islower(int c)
 {
-	/* Input character from user */
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
-	else 
+	else
 	{
-		return(0);
+		return (0);
 	}
 }
