015fd41a94a0301cac7dcd8046734e35d7f28004
Olu-D <tunsmen13@icloud.com>
2022-10-18 11:37:03 +0000

    Print _putchar with write and unistd.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "unistd.h"
+
+
+/**
+ * main- entry point. This program prints _putchar to stdout
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 10);
+
+	return (0);
+
+}
