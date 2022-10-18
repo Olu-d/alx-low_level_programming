2b028c8529faf61c18223e59bee19b7b073f468e
Olu-D <tunsmen13@icloud.com>
2022-10-18 17:23:06 +0000

    Printing alphabets n times using a user-define header file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_alphabet - This function prints the alphabets from a - z
+ *
+ * No main, so no return statement.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph); /*call _putchar function to print*/
+		}
+	
+	_putchar('\n'); /*new line at the end of the print*/
+
+	}
+}
