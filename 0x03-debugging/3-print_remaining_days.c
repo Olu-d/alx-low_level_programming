f62b14b37451b473fc7500ee05dce7cf57dd406a
Olu-D <tunsmen13@icloud.com>
2022-10-20 22:49:39 +0000

    Convert date to day of year and determine remaining days in the year, taking leap year into consideration

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 			(year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
 	{
 		/* if it has a day 29 in February) */
-		if (month >= 2 && day > 59)
+		if (month >= 2 && day > 60)
 		{
 			day++;
 		}
