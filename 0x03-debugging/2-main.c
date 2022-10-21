75faec983387fb963df56c3c61118313b2b3f551
Olu-D <tunsmen13@icloud.com>
2022-10-21 01:28:55 +0000

    Debugging largest of three integers

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = -972;
+        a = 8;
         b = 8;
-        c = -10;
+        c = 8;
 
         largest = largest_number(a, b, c);
 
