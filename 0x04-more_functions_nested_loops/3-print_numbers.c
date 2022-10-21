619297436654435ab236225a2c1a9313dc1dad14
Olu-D <tunsmen13@icloud.com>
2022-10-21 14:01:42 +0000

    Print 0 to 9 followed by newline with write

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_numbers - Print the numbers 1 to 9
+ * Return: The printed numbers followed by a new line
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		_putchar(num);
+		_putchar('\n');
+
+
+
+}
+
