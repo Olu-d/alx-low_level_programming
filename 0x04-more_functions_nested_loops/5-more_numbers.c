69802a6da31e05c816a13c3ed2c20e0981232145
Olu-D <tunsmen13@icloud.com>
2022-10-21 20:39:59 +0000

    Print 1-14 with write buffer 1 using %10 and /10 to append digits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,18 +12,17 @@ void more_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <=10; num++)
+	for (num = 0; num <= 10; num++)
 	{
 		int i = 0;
+
 		while (i <= 14)
 		{
 			if (i / 10 > 0)
 				_putchar((i / 10) + '0'); /*grab first digit if > 10 */
 			_putchar((i % 10) + '0'); /*grab last digit less than 10 */
 			i++;
-
 		}
-		
 		_putchar('\n');
 	}
 
