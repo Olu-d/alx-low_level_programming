421b2d0c09d52e0df10106ad86d654f68414e60b
Olu-D <tunsmen13@icloud.com>
2022-10-22 04:18:22 +0000

    Fizz Buzz FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,22 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s ", "FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s ", "Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s ", "Buzz");
+			printf("Buzz");
 		}
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		
+
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
