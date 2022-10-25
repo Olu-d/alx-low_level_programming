584da165653264249ff089e9f880cd8d43863cbb
Olu-D <tunsmen13@icloud.com>
2022-10-25 13:58:11 +0000

    Update the value of a pointer variable with function call

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98- updates the value of the variable n point to to 98
+ * @n : integer pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
