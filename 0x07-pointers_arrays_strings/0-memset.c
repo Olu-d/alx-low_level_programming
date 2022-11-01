1a8b2d5e44387210844025bdfc78463296ae8042
Olu-D <tunsmen13@icloud.com>
2022-11-01 16:58:04 +0000

    function fills the first n bytes of the memory area pointed to by s with the constant byte b

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _memset - writes the text in a buffer b to the address in s from 0 to n
+ * The official documentation says function fills the first n bytes of the
+ * memory area pointed to by s with the constant byte b
+ * @s: array address to point to
+ * @b: content to fill in buffer
+ * @n: number of bytes to fill
+ * Return: s - the array
+ *********/
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		s + 1;
+	}
+
+	return (s);
+}
+
