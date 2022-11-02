c98c31c76d57b5d51fbf865cab9699352b9141fa
Olu-D <tunsmen13@icloud.com>
2022-11-02 18:34:46 +0000

    a function that searches a string for any of a set of bytes. I learnt a new way return works here

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include"main.h"
+#include<string.h>
+#include<stdio.h>
+
+/**
+ * _strpbrk - The _strpbrk() function locates the first occurrence
+ * in the string s of any of the bytes in the string accept
+ * @s: The string to edit and return a pointer to
+ * @accept: The byte to test for matches in
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int s_len = strlen(s);
+	int accept_len = strlen(accept);
+
+	i = 0;
+
+	while (*s != '\0')
+	{
+		for (j = 0; j < accept_len; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+	s++;
+	i++;
+	}
+
+}
