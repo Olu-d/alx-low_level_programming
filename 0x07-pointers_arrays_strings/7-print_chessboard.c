ef052d432138713732b896a3bcfd8ad1cb6a213f
Olu-D <tunsmen13@icloud.com>
2022-11-03 00:21:13 +0000

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,11 +9,15 @@ void print_chessboard(char (*a)[8])
 
 	int i, j;
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%d%d" 
+			printf("%c", a[i][j]);
+			if (j == 7)
+				printf("\n");
+		}
+	}
 
 
 
