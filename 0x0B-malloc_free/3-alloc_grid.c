2252345364237f1464630f06664a98374a0dda04
Olu-D <tunsmen13@icloud.com>
2022-11-10 00:20:04 +0000

    return a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,27 +5,36 @@
 int **alloc_grid(int width, int height)
 {
 
-	int i, j; 
+	int i, j;
+	int **arr;
+
+	/*validate input. Remember input validation should come first*/
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
 	/*create a pointer to pointer of an array of ints*/
 	/*this represents the row level pointers i.e. vertical*/
-	int** arr = malloc(height * sizeof(int*));
+	arr = malloc(height * sizeof(int *));
 	/*check that malloc didn't fail*/
 	if (arr == NULL)
 		return (NULL);
 
 	/*this represents the width of the elements i.e. horizontal*/
 	for (i = 0; i < height; i++)
-	        arr[i] = malloc(width * sizeof(int*));
+	{
+		arr[i] = malloc(width * sizeof(int));
 	/*check that malloc didn't fail*/
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 
+	}
 
 	return (arr);
 }
