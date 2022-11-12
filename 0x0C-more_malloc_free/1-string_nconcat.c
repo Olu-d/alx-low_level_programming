b6c4fd3fd010ea0e5db4777d9960296385ec4715
Olu-D <tunsmen13@icloud.com>
2022-11-12 14:02:18 +0000

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,13 +2,22 @@
 #include<string.h>
 #include<stdlib.h>
 
+/**
+ * string_nconcat- Concatenate two strings
+ * @s1: String to concatenate to
+ * @s2: String to select n buffers from
+ * @n: Number of bytes to copy from s2
+ * Return: pointer to the allocated memory
+ */
+
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
 
 
 	/*allocate space for concatenation*/
-	concat = malloc((strlen(s1) + strlen(s2)) * sizeof *concat + 1);
+	concat = malloc((strlen(s1) + strlen(s2)) * sizeof(concat) + 1);
 
 	/*if function fails, return NULL*/
 	if (concat == NULL)
