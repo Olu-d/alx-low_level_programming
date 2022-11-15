d1a11d714d25a7c4b147a44519537f20f625fead
Olu-D <tunsmen13@icloud.com>
2022-11-15 22:10:12 +0000

    Print the name of the file compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- Returns the name of the file the function is compiled from
+ * Return: 0: success
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+	return (EXIT_SUCCESS);
+
+}
