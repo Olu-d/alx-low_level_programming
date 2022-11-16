d09785b2299060704c32e4a5873bb13872b49ffa
Olu-D <tunsmen13@icloud.com>
2022-11-16 20:42:58 +0000

    Create new struct variable using function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include"dog.h"
+
+/**
+ * new_dog - initializing a new variable with type dog_t
+ * @name: member of new_dog
+ * @age: member 
+ * @owner: member
+ * Return: pointer to new variable
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *inscope_dog;
+	
+	inscope_dog = malloc(sizeof(dog_t));
+	if (inscope_dog == NULL)
+		return(NULL);
+
+	inscope_dog->name = name;
+	inscope_dog->age = age;
+	inscope_dog->owner = owner;
+
+	return (inscope_dog);
+}
+
