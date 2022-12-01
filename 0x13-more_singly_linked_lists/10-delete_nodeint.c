44a72fc9133a892578275bba9105a7570467ec3a
Olu-D <tunsmen13@icloud.com>
2022-12-01 21:15:54 +0000

    insert new node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,6 +9,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *deleted_value;
 	unsigned int count = 0;
 	unsigned int prev_index = (index - 1);
 	listint_t *temp = *head;
@@ -19,6 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp->next;
+		free(temp);
 		return (1);
 	}
 
@@ -26,7 +28,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (count == prev_index)
 		{
+			deleted_value = temp->next;
 			temp->next = temp->next->next;
+			free(deleted_value);
 			return (1);
 		}
 		count++;
