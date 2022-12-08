b0498363d58f43b35144c8a62f15fd52e94e9c8c
Olu-D <tunsmen13@icloud.com>
2022-12-08 02:59:47 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,14 @@
 #include<stdlib.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * create_file - A function that creates a file
  * @filename: Name of the file to create
- * @text_content: NULL terminated string to write to file
+ * @text_context: NULL terminated string to write to file
  *
  * Return: 1 on success, -1 on failure
  * -1 if filename is NULL
@@ -13,5 +18,21 @@ int create_file(const char *filename, char *text_context)
 {
 	int fd;
 
+	if (filename == NULL)
+		return (-1);
+
+
 	/* open the file */
-	fd = open(filename
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if(fd == -1)
+		return (-1);
+
+	write(fd, text_context, strlen(text_context));
+
+	/*write(2, text_context, strlen(text_context));*/
+
+	close (fd);
+
+	return (1);
+}
